24c3ae06c0d7e7980a73d888ea110cddeb5cb277
Steven Burgart <skburgart@gmail.com>
2014-12-27 20:45:45 -0500

    More coords

--- Coords.py
+++ Coords.py
@@ -1,5 +1,4 @@
 class Coords():
-
     # padding
     PAD_X = 200
     PAD_Y = 238
@@ -13,3 +12,22 @@ class Coords():
     START_PLAY = (300, 200)
     START_SKIP = (572, 450)
     START_CONTINUE = (314, 388)
+
+    # Food
+    FOOD_SUSHI = (40, 330)
+    FOOD_RICE = (90, 330)
+    FOOD_NORI = (40, 390)
+    FOOD_ROE = (90, 390)
+    FOOD_SALMON = (40, 450)
+    FOOD_UNAGI = (90, 450)
+
+    # Phone
+    PHONE = (550, 350)
+    PHONE_TOPPING = (500, 271)
+    PHONE_TOPPING_SHRIMP = (492, 224)
+    PHONE_TOPPING_UNAGI = (580, 224)
+    PHONE_TOPPING_NORI = (492, 277)
+    PHONE_TOPPING_ROE = (580, 277)
+    PHONE_TOPPING_SALMON = (490, 332)
+    PHONE_RICE = (500, 295)
+    PHONE_SAKE = (500, 319)
