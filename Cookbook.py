d383f3b614316b0d44dec149e836b251bd17c0a5
Steven Burgart <skburgart@gmail.com>
2014-12-27 21:05:00 -0500

    Added cookbook

---
+++ Cookbook.py
@@ -0,0 +1,17 @@
+class Cookbook(object):
+    recipe = {
+        'onigiri': {
+            'rice': 2,
+            'nori': 1
+        },
+        'california': {
+            'rice': 1,
+            'nori': 1,
+            'roe': 1
+        },
+        'gunkan': {
+            'rice': 1,
+            'nori': 1,
+            'roe': 2
+        }
+    }
--- Inventory.py
+++ Inventory.py
@@ -1,7 +1,9 @@
 class Inventory(object):
-    stock = {'sushi': 5,
-             'rice': 10,
-             'nori': 10,
-             'roe': 10,
-             'salmon': 5,
-             'unagi': 5}
+    stock = {
+        'sushi': 5,
+        'rice': 10,
+        'nori': 10,
+        'roe': 10,
+        'salmon': 5,
+        'unagi': 5
+    }
